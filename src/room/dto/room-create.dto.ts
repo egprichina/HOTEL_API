10ee6e08f2e3e856c3fd7779f70dc83263fef02f
prichinae <prichinae@gmail.com>
2024-10-18 10:02:49 +0300

    Выполнение задания 2(Тесты)

--- src/room/dto/room-create.dto.ts
+++ src/room/dto/room-create.dto.ts
@@ -1,5 +1,10 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class RoomCreateDto {
+	@IsNumber()
 	number: number;
+	@IsString()
 	descrRoom: string;
+	@IsString()
 	type: string;
 }
---
+++ test/reserv.app.e2e-spec.ts
@@ -0,0 +1,56 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { ReservCreateDto } from '../src/reserv/dto/reserv-create.dto';
+import { disconnect } from 'mongoose';
+
+const testDtoReserv: ReservCreateDto = {
+	date: new Date('2024-01-01'),
+	room: 28,
+	man: 'Иванов Иван Иванович',
+};
+
+describe('AppController (e2e)', () => {
+	let app: INestApplication;
+	let createdId: string;
+
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
+
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
+
+	// it('/ (GET)', () => {
+	// 	return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+	// });
+
+	// Тест бронирования
+	it('/reserv (POST) - success', async () => {
+		return await request(app.getHttpServer())
+			.post('/reserv')
+			.send(testDtoReserv)
+			.expect(201)
+			.then(({ body }: request.Response) => {
+				createdId = body._id;
+				expect(createdId).toBeDefined();
+			});
+	});
+
+	it('/reserv (POST) - fail', async () => {
+		return await request(app.getHttpServer()).post('/reserv').send(testDtoReserv).expect(400);
+	});
+
+	it('/reserv/:id (DELETE) - success', () => {
+		return request(app.getHttpServer())
+			.delete('/reserv/' + createdId)
+			.expect(200);
+	});
+
+	afterAll(() => {
+		disconnect();
+	});
+});
